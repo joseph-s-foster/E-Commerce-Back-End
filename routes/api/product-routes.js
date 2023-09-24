d3a6c661d1c6330a2d20f39a40e345bd64d5fa1c
D3C0D1NG <joseph.s.foster@icloud.com>
2023-09-24 12:15:50 -0500

    added delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,7 +111,7 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
-    const deletedProduct = await Tag.destroy({
+    const deletedProduct = await Product.destroy({
       where: {
         id: req.params.id,
       },
