eea11991df10ae8590459a106f3810c01dd0047c
D3C0D1NG <joseph.s.foster@icloud.com>
2023-09-24 09:26:52 -0500

    adding tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,7 +3,15 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Tag.findAll({
+      include: [{ model: Product}, { model: ProductTag }],
+    });
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
   // find all tags
   // be sure to include its associated Product data
   // Tag.findAll({
