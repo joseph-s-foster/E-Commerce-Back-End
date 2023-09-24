0120e5ee5ba8c47be7400865752ae67b952109f4
D3C0D1NG <joseph.s.foster@icloud.com>
2023-09-24 09:29:51 -0500

    adding tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,41 +1,51 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
-  try {
-    const productData = await Tag.findAll({
-      include: [{ model: Product}, { model: ProductTag }],
-    });
-  res.status(200).json(productData);
-} catch (err) {
-  res.status(500).json(err);
-}
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  // Tag.findAll({
-  //   include: [
-  //     {
-  //       model: Product,
-  //       through: ProductTag,
-  //     },
+
+  try {
+    const productData = await Tag.findAll({
+      include: [{ model: Product }, { model: ProductTag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  
+  try {
+    const productData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
