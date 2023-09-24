03caf54f726f7d18ed31236ee2be0e106f8e35d2
D3C0D1NG <joseph.s.foster@icloud.com>
2023-09-24 09:36:47 -0500

    adding tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  
+
   try {
     const productData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }, { model: ProductTag }],
@@ -40,15 +40,34 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new tag
   try {
-  const productData = await Tag.create(req.body);
-  res.status(200).json(productData);
+    const productData = await Tag.create(req.body);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Tag updated successfully." });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
